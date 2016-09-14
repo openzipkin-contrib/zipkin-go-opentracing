ae8254c8cf5ac80f2dc4ed87307f9fe8f7ba8e50
clchen <clchen@leyantech.com>
2016-09-14 12:33:02 +0800

    add comments

--- collector-http.go
+++ collector-http.go
@@ -10,8 +10,11 @@ import (
 )
 
 const defaultMaxAsyncQueueSize = 64
+
+// Default timeout for http request in seconds
 const defaultHttpTimeout = time.Second * 5
 
+// HttpCollector implements Collector by forwarding spans to a http server.
 type HttpCollector struct {
 	logger          Logger
 	url             string
@@ -23,18 +26,27 @@ type HttpCollector struct {
 // HttpOption sets a parameter for the HttpCollector
 type HttpOption func(c *HttpCollector)
 
+// HttpLogger sets the logger used to report errors in the collection
+// process. By default, a no-op logger is used, i.e. no errors are logged
+// anywhere. It's important to set this option in a production service.
 func HttpLogger(logger Logger) HttpOption {
 	return func(c *HttpCollector) { c.logger = logger }
 }
 
+// HttpMaxASyncQueueSize sets the maximum buffer size for the async queue.
 func HttpMaxAsyncQueueSize(size int) HttpOption {
 	return func(c *HttpCollector) { c.asyncInputQueue = make(chan *zipkincore.Span, size) }
 }
 
+// HttpTimeout sets maximum timeout for http request.
 func HttpTimeout(duration time.Duration) HttpOption {
 	return func(c *HttpCollector) { c.client.Timeout = duration }
 }
 
+// NewHttpCollector returns a new Http-backend Collector. url should be a http
+// url for handle post request. timeout is passed to http client. queueSize control
+// the maximum size of buffer of async queue. The logger is used to log errors,
+// such as send failures;
 func NewHttpCollector(url string, options ...HttpOption) (Collector, error) {
 	c := &HttpCollector{
 		logger:          NewNopLogger(),
@@ -51,11 +63,13 @@ func NewHttpCollector(url string, options ...HttpOption) (Collector, error) {
 	return c, nil
 }
 
+// Collect implements Collector.
 func (c *HttpCollector) Collect(s *zipkincore.Span) error {
 	c.asyncInputQueue <- s
 	return nil
 }
 
+// Close implements Collector.
 func (c *HttpCollector) Close() error {
 	c.quit <- struct{}{}
 	return nil

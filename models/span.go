94b10cac2a03b11054c3db985abfa28453519a52
stocco <renato.stoco@gmail.com>
2019-05-05 13:45:24 -0300

    correct json names

--- models/span.go
+++ models/span.go
@@ -2,19 +2,27 @@ package models
 
 
 type Span struct {
-	TraceID string `json:"trace_id"`
+	TraceID string `json:"traceId"`
 	// unused field # 2
 	Name string `json:"name"`
 	ID string `json:"id"`
-	ParentID string `json:"parent_id,omitempty"`
+	ParentID string `json:"parentId,omitempty"`
 	Debug bool `json:"debug,omitempty"`
 	Timestamp int64 `json:"timestamp,omitempty"`
 	Duration int64 `json:"duration,omitempty"`
-	TraceIDHigh string `json:"trace_id_high,omitempty"`
-	BinaryAnnotations []*BinaryAnnotation `json:"binary_annotations"`
+	TraceIDHigh string `json:"traceIdHigh,omitempty"`
+	BinaryAnnotations []*BinaryAnnotation `json:"binaryAnnotations"`
 }
 
 type BinaryAnnotation struct {
 	Key string `json:"key"`
 	Value string `json:"value"`
+	Endpoint Endpoint `json:"endpoint"`
+}
+
+type Endpoint struct {
+	Ipv4 string `json:"ipv4"`
+	Port int64  `json:"port"`
+	ServiceName string `json:"serviceName"`
+	Ipv6 string `json:"ipv6,omitempty"`
 }

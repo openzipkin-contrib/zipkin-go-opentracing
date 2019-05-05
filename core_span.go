099cc283ce05718e7986f0e9e50219760b09db1e
stocco <renato.stoco@gmail.com>
2019-05-05 18:41:38 -0300

    added comment to core span

--- core_span.go
+++ core_span.go
@@ -1,5 +1,6 @@
 package zipkintracer
 
+// CoreSpan represents the span to be sent to the zipkin server
 type CoreSpan struct {
 	TraceID           string                  `json:"traceId"`
 	Name              string                  `json:"name"`
@@ -13,18 +14,21 @@ type CoreSpan struct {
 	BinaryAnnotations []*CoreBinaryAnnotation `json:"binaryAnnotations"`
 }
 
+// CoreBinaryAnnotation represents the tags added in the span
 type CoreBinaryAnnotation struct {
 	Key      string       `json:"key"`
 	Value    string       `json:"value"`
 	Endpoint CoreEndpoint `json:"endpoint"`
 }
 
+// CoreAnnotation represents the tags that are inherited from parent spans
 type CoreAnnotation struct {
 	Timestamp int64         `json:"timestamp"`
 	Value     string        `json:"value"`
 	Host      *CoreEndpoint `json:"host,omitempty"`
 }
 
+// CoreEndpoint represents the tags that are applied to all spans from the given service.
 type CoreEndpoint struct {
 	Ipv4        string `json:"ipv4"`
 	Port        int16  `json:"port"`
